4e300c9b251bf7edc724d5fa7efd00eba5b6f8bd
pesesito <40332795+pesesito@users.noreply.github.com>
2021-03-30 22:19:03 +0200

    Delete Hellscape directory

--- Hellscape/scripts/Path2D.gd
+++
@@ -1,6 +0,0 @@
-extends Path2D
-
-var speed = 210
-
-func _physics_process(delta):
-	$PathFollow2D.set_offset($PathFollow2D.get_offset()+speed*delta)
--- Hellscape/scripts/saw.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-# Spini boi
-func _process(_delta):
-	$saw.rotation += 1
