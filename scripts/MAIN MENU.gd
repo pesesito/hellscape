36887d014ad1bb53dc8863fe73b0aab62a9f91e0
pesesito <40332795+pesesito@users.noreply.github.com>
2021-04-04 11:47:48 +0200

    Optimization Update.

--- scripts/MAIN MENU.gd
+++ scripts/MAIN MENU.gd
@@ -1,4 +1,5 @@
 extends Node
 
 func _on_startButton_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/Levels/Level 0.tscn")
--- scripts/saw.gd
+++ scripts/saw.gd
@@ -3,3 +3,13 @@ extends Area2D
 # Spini boi
 func _process(_delta):
 	$saw.rotation += 1
+
+# warning-ignore:unused_argument
+func _on_VisibilityNotifier2D_viewport_entered(viewport):
+	$Light2D.enabled = true
+	$saw.show()
+
+# warning-ignore:unused_argument
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	$Light2D.enabled = false
+	$saw.hide()
